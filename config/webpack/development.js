181e686d73a255c36db04c094ea6a9031241d7ca
Justin <justinbkay@gmail.com>
2018-11-18 09:08:23 -0700

    get css and images loading through webpack

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,4 +2,9 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
 
+const sassLoader = environment.loaders.get('sass')
+const sassEl = sassLoader.use.find((element) => {
+  return element.loader === 'sass-loader'
+})
+
 module.exports = environment.toWebpackConfig()
