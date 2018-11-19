87096adde750e666f6879bd839c47da2352d0aa8
Justin <justinbkay@gmail.com>
2018-11-19 09:12:48 -0700

    style alert and notices

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,3 +9,34 @@
 
 require.context('../stylesheets/', true, /^\.\/[^_].*\.(css|scss|sass)$/i)
 require.context('../images/', true, /\.(gif|jpg|png|svg)$/i)
+
+document.addEventListener('DOMContentLoaded', function() {
+  let notice = document.getElementsByClassName('notice-wrapper')[0];
+  let alert = document.getElementsByClassName('alert-wrapper')[0];
+  setTimeout(function () {
+    if (notice) {
+      var fadeEffect = setInterval(function () {
+        if (!notice.style.opacity) {
+            notice.style.opacity = 1;
+        }
+        if (notice.style.opacity > 0) {
+            notice.style.opacity -= 0.1;
+        } else {
+            clearInterval(fadeEffect);
+        }
+      }, 30);
+    }
+    if (alert) {
+      var fadeEffect = setInterval(function () {
+        if (!alert.style.opacity) {
+            alert.style.opacity = 1;
+        }
+        if (alert.style.opacity > 0) {
+            alert.style.opacity -= 0.1;
+        } else {
+            clearInterval(fadeEffect);
+        }
+      }, 30);
+    }
+  }, 5000)
+}, false);
