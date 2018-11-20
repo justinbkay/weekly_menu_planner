60c54283000c8bfb9c62482d5fbf642d6dd6c1d8
Justin <justinbkay@gmail.com>
2018-11-20 07:19:22 -0700

    stub devise auth to make spec pass

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -4,6 +4,10 @@ RSpec.describe HomeController, type: :controller do
 
   describe "GET #index" do
     it "returns http success" do
+      user = double('user')
+      allow(request.env['warden']).to receive(:authenticate!).and_return(user)
+      allow(controller).to receive(:current_user).and_return(user)
+
       get :index
       expect(response).to have_http_status(:success)
     end
