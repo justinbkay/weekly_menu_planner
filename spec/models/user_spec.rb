51ad560bd5d7f6da061481d5835a0e4dfeeda533
Justin <justinbkay@gmail.com>
2018-11-20 07:19:43 -0700

    add user specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'requires and email to be valid' do
+    user = User.new
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
+  end
+
+  it 'requires a valid email address' do
+    user = User.new(email: 'someone@example.com')
+    expect(user.errors[:email]).to eq([])
+  end
+
+  it 'requires a first name' do
+    user = User.new
+    user.valid?
+    expect(user.errors[:first_name]).to include("can't be blank")
+  end
+
+  it 'requires a last name' do
+    user = User.new
+    user.valid?
+    expect(user.errors[:last_name]).to include("can't be blank")
+  end
 end
