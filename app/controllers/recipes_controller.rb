087ccce4644a41962b807635efbf6e132b912de3
Justin <justinbkay@gmail.com>
2018-11-20 11:58:43 -0700

    adding the rest of recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,13 +1,45 @@
 class RecipesController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    @recipes = current_user.recipes
+  end
+
   def new
     @recipe = current_user.recipes.build
   end
 
+  def create
+    @recipe = current_user.recipes.build(recipe_params)
+
+    if @recipe.save
+      redirect_to user_recipes_path
+    else
+      render :new
+    end
+  end
+
   def edit
+    @recipe = current_user.recipes.find(params[:id])
+  end
+
+  def update
+    @recipe = current_user.recipes.find(params[:id])
+
+    if @recipe.update(recipe_params)
+      redirect_to user_recipes_path
+    else
+      render :edit
+    end
   end
 
   def show
+    @recipe = current_user.recipe.find(params[:id])
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :description, :ingredients, :steps)
   end
 end
